50bd260c521f477213b82843e215f38c7487365d
Bec Owens <rowe0205@uni.sydney.edu.au>
2025-09-23 20:19:36 +1000

    add scatter plot

--- app.R
+++ app.R
@@ -8,3 +8,12 @@ finaldf <- df_cleaned
 
 cat_vars <- names(df)[sapply(df, is.factor)]
 num_vars <- names(df)[sapply(df, is.numeric)]
+
+server <- function(input, output, session) {
+  output$cat_plot <- renderPlot({
+    req(input$cat_var1, input$cat_var2)
+    ggplot(df, aes_string(x = input$cat_var1, fill = input$cat_var2)) +
+      geom_bar(position = "dodge") +
+      labs(x = input$cat_var1, fill = input$cat_var2) +
+      theme_minimal()
+  })}
